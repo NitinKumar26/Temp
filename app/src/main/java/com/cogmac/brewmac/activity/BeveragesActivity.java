9d86ef88cfffe8cc50e3e26f4fb343b7a42be870
Nitin Kumar <nk037578>
2019-06-17 17:43:30 +0530

    17.06

--- app/src/main/java/com/cogmac/brewmac/activity/BeveragesActivity.java
+++ app/src/main/java/com/cogmac/brewmac/activity/BeveragesActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.cogmac.brewmac.R;
 
 public class BeveragesActivity extends AppCompatActivity {
